be51b50ce6a3e6f8ea30aed5a6f4eba64df1fe4f
Ruchitha K P <ruchithakp74@gmail.com>
2026-02-22 14:20:35 +0530

    Integrate Google Analytics into RootLayout
    Added Google Analytics scripts to the head section.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
+import Script from "next/script";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import './globals.css';
@@ -12,6 +13,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WL7QRD7PXB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WL7QRD7PXB');
+          `}
+        </Script>
+      </head>
+
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="flex flex-col min-h-screen">
